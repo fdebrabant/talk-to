423e0ab901eb3ea9dd0fa510950d5fbc162950e8
f.debrabant <f.debrabant@groupeonepoint.com>
2024-03-28 10:41:35 +0100

    refactor(gameService)

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -1,14 +1,19 @@
-import {Data} from "../data/Data.ts"
+import {Data, DataItem} from "../data/Data.ts"
+
+type Question = string;
+type Answer = string;
 
 interface Message {
-    questions: string[];
-    answers: string[]
+    questions: Question[];
+    answers: Answer[]
 }
 
 export class GameService {
     public conversation: string[] = [];
+
     constructor(private data: Data) {
     }
+
 
     firstMessage(): Message | undefined {
         const startItem = this.data.find(dataItem => dataItem.name === "Start");
@@ -17,16 +22,22 @@ export class GameService {
             return;
         }
 
-        const answers = startItem.source
-            .match(/\[\[(\w+)\]\]/g)
-            ?.map(answer => answer.replace(/\[|\[|\]|\]/g, ''))
+        const answers = this.generateAnswers(startItem.source);
+        const questions = this.generateQuestions(startItem.source)
 
-        const questions = startItem.source
+        return {questions, answers};
+    }
+
+    private generateAnswers(dataItemSource: DataItem["source"]): Answer[] {
+        return dataItemSource
+            .match(/\[\[(\w+)\]\]/g)
+            ?.map(answer => answer.replace(/\[|\[|\]|\]/g, '')) || [];
+    }
+
+    private generateQuestions(dataItemSource: DataItem["source"]): Question[] {
+        return dataItemSource
             .split('[')[0]
             .trim()
             .split('\n').filter(question => question);
-
-        return {questions, answers: answers || []};
     }
-
 }
