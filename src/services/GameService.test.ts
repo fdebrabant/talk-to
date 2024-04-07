625ba46280e4cef922884f9d581cc7cfbd452149
fdebrabant <debrabant.fanny@gmail.com>
2024-04-07 15:13:40 +0200

    format(service)

--- src/services/GameService.test.ts
+++ src/services/GameService.test.ts
@@ -1,28 +1,28 @@
-import {GameService} from "./GameService.ts";
-import data from '../data/data.json'
+import { GameService } from "./GameService.ts";
+import data from "../data/data.json";
 
-describe('GameService', () => {
-    test('should be instance of GameService', () => {
-        const gameService = new GameService(data);
-        expect(gameService).toBeInstanceOf(GameService)
+describe("GameService", () => {
+  test("should be instance of GameService", () => {
+    const gameService = new GameService(data);
+    expect(gameService).toBeInstanceOf(GameService);
+  });
+
+  test("When instantiate game, conversation should be empty ", () => {
+    const gameService = new GameService(data);
+    expect(gameService.conversation).toEqual([]);
+  });
+
+  test("should return first message", () => {
+    const gameService = new GameService(data);
+    const firstMessage = gameService.firstMessage();
+    expect(firstMessage).toEqual({
+      questions: ["What's your favorite color?"],
+      answers: ["Purple", "Yellow", "Pink"],
     });
+  });
 
-    test('When instantiate game, conversation should be empty ', () => {
-        const gameService = new GameService(data);
-        expect(gameService.conversation).toEqual([])
-    });
-
-    test('should return first message', () => {
-        const gameService = new GameService(data);
-        const firstMessage = gameService.firstMessage();
-        expect(firstMessage).toEqual({
-            questions: ["What's your favorite color?"],
-            answers: ["Purple", "Yellow", "Pink"]
-        })
-    })
-
-    test('should return error when first message doesnt exist', () => {
-        const gameService = new GameService([]);
-        expect(() => gameService.firstMessage()).toThrow('Start Item doesnt exist')
-    })
+  test("should return error when first message doesnt exist", () => {
+    const gameService = new GameService([]);
+    expect(() => gameService.firstMessage()).toThrow("Start Item doesnt exist");
+  });
 });
--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -1,43 +1,44 @@
-import {Data, DataItem} from "../data/Data.ts"
+import { Data, DataItem } from "../data/Data.ts";
 
 type Question = string;
 type Answer = string;
 
 interface Message {
-    questions: Question[];
-    answers: Answer[]
+  questions: Question[];
+  answers: Answer[];
 }
 
 export class GameService {
-    public conversation: string[] = [];
+  public conversation: string[] = [];
 
-    constructor(private data: Data) {
+  constructor(private data: Data) {}
+
+  firstMessage(): Message {
+    const startItem = this.data.find((dataItem) => dataItem.name === "Start");
+
+    if (!startItem || !startItem.source) {
+      throw new Error("Start Item doesnt exist");
     }
 
+    const answers = this.generateAnswers(startItem.source);
+    const questions = this.generateQuestions(startItem.source);
 
-    firstMessage(): Message {
-        const startItem = this.data.find(dataItem => dataItem.name === "Start");
+    return { questions, answers };
+  }
 
-        if (!startItem || !startItem.source) {
-            throw new Error('Start Item doesnt exist');
-        }
+  private generateAnswers(dataItemSource: DataItem["source"]): Answer[] {
+    return (
+      dataItemSource
+        .match(/\[\[(\w+)\]\]/g)
+        ?.map((answer) => answer.replace(/\[|\[|\]|\]/g, "")) || []
+    );
+  }
 
-        const answers = this.generateAnswers(startItem.source);
-        const questions = this.generateQuestions(startItem.source)
-
-        return {questions, answers};
-    }
-
-    private generateAnswers(dataItemSource: DataItem["source"]): Answer[] {
-        return dataItemSource
-            .match(/\[\[(\w+)\]\]/g)
-            ?.map(answer => answer.replace(/\[|\[|\]|\]/g, '')) || [];
-    }
-
-    private generateQuestions(dataItemSource: DataItem["source"]): Question[] {
-        return dataItemSource
-            .split('[')[0]
-            .trim()
-            .split('\n').filter(question => question);
-    }
+  private generateQuestions(dataItemSource: DataItem["source"]): Question[] {
+    return dataItemSource
+      .split("[")[0]
+      .trim()
+      .split("\n")
+      .filter((question) => question);
+  }
 }
